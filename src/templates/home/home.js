6ac306029049002b85735c6a92ba3eeec4d8e76d
Ben Tran <ben.tran@liti.ws>
2014-08-16 02:11:37 +0700

    header

--- src/templates/home/home.js
+++ src/templates/home/home.js
@@ -1,6 +1,17 @@
 mapp.controller('homeController', function($scope){
 	$scope.message = "This is Home page";
 });
+mapp.directive('topNav', function(){
+  function link(scope, element, attrs) {
+
+    }
+    return {
+      restrict: 'E',
+      templateUrl: 'templates/com/header.tpl.html',
+      link: link,
+      scope: true
+    };
+});
 mapp.directive('footerNav', function(){
 	function link(scope, element, attrs) {
 
